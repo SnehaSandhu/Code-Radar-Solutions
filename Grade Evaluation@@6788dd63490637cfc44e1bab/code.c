f3fd6d705f7dcf570cd8e2a0ab3aa6bc41f7220a
SnehaSandhu <sneha1692.becse24@chitkara.edu.in>
2025-02-05 20:12:49 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -4,23 +4,23 @@ int main()
     int, ch;
     printf("");
     scanf("%d",&ch);
-    if (ch='A')
+    if (ch=='A')
     {
         printf(Excellent);
     }
-    else if (ch='B')
+    else if (ch=='B')
     {
         printf("Good");
     }
-    else if(ch='C')
+    else if(ch=='C')
     {
         printf("Average");
     }
-    else if(ch='D')
+    else if(ch=='D')
     {
         printf("Below Average");
     }
-    else if(ch='F')
+    else if(ch=='F')
     {
         printf("Fail");
     }
